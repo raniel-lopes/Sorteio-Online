0c2c7a49af98bca7cb00b53327a1160b77d3820d
Raniel Lopes <raniel.ldms@gmail.com>
2025-07-26 12:28:32 -0300

    Migrations para correção de erro no id da rifa

---
+++ backend/migrations/20250726221000-add-rifaId-to-participantes.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn('Participantes', 'rifaId', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Rifas',
+        key: 'id'
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE'
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn('Participantes', 'rifaId');
+  }
+};
